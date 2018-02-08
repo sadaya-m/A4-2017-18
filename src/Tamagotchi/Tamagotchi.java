8480e41aef867127173da01fc85072d36772ee62
sadaya-m <sadaya-m@1W07-10.uwds.uwinnipeg.ca>
2018-02-08 10:15:38 -0600

    Added Tamagotchi properties and made tests of txt file in client code

--- src/Tamagotchi/Tamagotchi.java
+++ src/Tamagotchi/Tamagotchi.java
@@ -1,13 +1,15 @@
 package Tamagotchi;
 
+import javax.swing.JOptionPane;
+
 /* **********************************************************
  * Programmer:	Ma Pauline Sadaya
  * Class:	CS30S
  * 
  * Assignment:	Assignment 4 Tamagotchi
  *
- * Description:	tamagotchi class to demonstrate inheritance and behaviours of
-tamagotchi objects
+ * Description:	Tamagotchi super class to demonstrate inheritance and behaviours of
+                tamagotchi objects derived from this class
  * *************************************************************/
  
  // import files here as needed
@@ -17,14 +19,139 @@ tamagotchi objects
  {  // begin class
  	
     // *********** class constants ********** 
-
+     
+        public enum happinessLevel{
+            Sad, Ok, Happy;
+	}//end enum happinessLevel
+        
+        public enum hungerLevel{
+            Low, Medium, High;
+        }//end enum hungerLevel
 
     // ********** instance variable **********
+     
+        private String name;
+        private int age;
+        private happinessLevel Happy = null;
+        private hungerLevel Hunger = null;
+        private boolean sick;
 
     // ********** constructors ***********
+        
+        /************************************
+        * Purpose: default constructor
+        * Interface:
+        *   in: none
+        *   out: none
+        ************************************/
+        public Tamagotchi(){
+        }//end default constructor
+     
+        /************************************
+        * Purpose: initialized constructor
+        * Interface:
+        *   in:     String Name
+        *           int age
+        *           happinessLevel Happy
+        *           hungerLevel Hunger
+        *           boolean sick
+        *   out: none
+        ************************************/
+        public Tamagotchi(String n, int a, happinessLevel h, hungerLevel hu, boolean s){
+            name = n;
+            age = a;
+            Happy = h;
+            Hunger = hu;
+            sick = s;
+        }// end initialized constructor
 
     // ********** accessors **********
+    
+        /*****************************************************
+         Purpose: get name of tamagotchi
+         Interface: IN: none							
+         Returns: name
+         *****************************************************/
+        public String getName() {
+            return name;
+        }
 
+        /*****************************************************
+         Purpose: get age of tamagotchi
+         Interface: IN: none							
+         Returns: age
+         *****************************************************/        
+        public int getAge() {
+            return age;
+        }
+        
+        /*****************************************************
+         Purpose: get happiness level of tamagotchi
+         Interface: IN: none							
+         Returns: happiness level
+         *****************************************************/
+        public happinessLevel getHappy() {
+            return Happy;
+        }
+
+        /*****************************************************
+         Purpose: get hunger level of tamagotchi
+         Interface: IN: none							
+         Returns: hunger level
+         *****************************************************/
+        public hungerLevel getHunger() {
+            return Hunger;
+        }
+
+        /*****************************************************
+         Purpose: determine if tamagotchi is sick
+         Interface: IN: none							
+         Returns: happiness level
+         *****************************************************/
+        public boolean isSick() {
+            return sick;
+        }
+        
     // ********** mutators **********
+
+        /*****************************************************
+         Purpose: Tamagotchi eats something and relevant changes are
+            made to the animals state
+         Interface: IN: none
+         Returns: none
+        *****************************************************/	 
+	 public void eat(){
+            if(Hunger == hungerLevel.Low){
+                JOptionPane.showMessageDialog(null, "Feed me! I'm starving");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Food is good. Food is life");
+            }
+	} // end eat
+         
+        /*****************************************************
+         Purpose: Tamagotchi is sick
+         Interface: IN: none
+         Returns: none
+        *****************************************************/	 
+	 public void sick(){
+            if(sick == true){
+                JOptionPane.showMessageDialog(null, "Cough cough.. Sniff..");
+            }
+	} // end eat
+         
+         /*****************************************************
+         Purpose: Tamagotchi is either sad,ok,or happy
+         Interface: IN: none
+         Returns: none
+        *****************************************************/	 
+	 public void happy(){
+            if(Happy == happinessLevel.Happy){
+                JOptionPane.showMessageDialog(null, "Let's play!");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "...");
+            }
+	} // end eat
  
  }  // end class
--- src/TamagotchiClient/TamagotchiClient.java
+++ src/TamagotchiClient/TamagotchiClient.java
@@ -14,27 +14,37 @@ package TamagotchiClient;
 //  Output:		describe the result of your program
 // ***********************************************************************
 
+import Tamagotchi.Tamagotchi;
+import Tamagotchi.Tamagotchi.happinessLevel;
+import Tamagotchi.Tamagotchi.hungerLevel;
+import java.io.*;
 import javax.swing.*;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class TamagotchiClient
 {  // begin class
-    public static void main(String args[])
+    public static void main(String args[]) throws IOException
     {  // begin main
     // ***** declaration of constants *****
 
     // ***** declaration of variables *****
         
         String strout;
+        String strin;
+        String delim = "[ :]+";		// delimiter string for splitting input string
+        String[] tokens = null;          // array for splitting input
 
     // ***** create objects *****
 
         ProgramInfo programInfo = new ProgramInfo();
+        BufferedReader fin = new BufferedReader(new FileReader("tamagotchiList.txt"));
+        ArrayList<Tamagotchi> Pet = new ArrayList<Tamagotchi>();
 
     // ***** create input stream *****
 
         //ConsoleReader console = new ConsoleReader(System.in);
-
+        
     // ***** Print Banner *****
 
         strout = programInfo.getBanner("Assignment 4 Tamagotchi");
@@ -48,10 +58,20 @@ public class TamagotchiClient
 
     // ***** processing *****
 
+        strin = fin.readLine();
+        
+        while(strin != null){
+            tokens = strin.split(delim);        //split string into tokens
+            happinessLevel t = happinessLevel.valueOf(tokens[2]);
+            hungerLevel h = hungerLevel.valueOf(tokens[3]);
+            Boolean b = Boolean.valueOf(tokens[4]);
+            Pet.add(new Tamagotchi(tokens[0], Integer.parseInt(tokens[1]), t, h, b));
+            strin = fin.readLine();
+        }//end eof loop
 
     // ***** output *****
 
-        // all formatted ouput is printed in this section
+        System.out.println(Pet.get(0).isSick());
 
     // ***** closing message *****
 
